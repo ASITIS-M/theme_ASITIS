27e4ccd303cbe843ab7ce62d49654d60f46395d3
Filip Kratos <filkra@centrum.cz>
2024-10-22 15:15:55 +0200

    Oprava předchozího (další)

--- R/colors_ASITIS.R
+++ R/colors_ASITIS.R
@@ -29,13 +29,12 @@ get_colors_ASITIS <- function(n = NA, dark = F) {
     "Barevná škála" = c("#D46616", "#d8645a", "#bb5b83", "#7b6da0", "#457db0", "#389672", "#8aa62e", "#eabe3b")
   )
   
-  
-  if (!dark & n > 0 & n < 9) {
+  if(is.na(n)){
+    return(colors_ASITIS)
+  } else if (!dark & n > 0 & n < 9) {
     return(colors_ASITIS$`Osmibarevná paleta světlá`[1:n])
   } else if (dark & n > 0 & n < 9) {
     return(colors_ASITIS$`Osmibarevná paleta tmavá`[1:n])
-  } else if (is.na(n)) {
-    return(colors_ASITIS)
   } else {
     stop("The required number of colors is not available. Choose a value between 1 and 8 or leave the value blank to get the full list of palettes.")
   }
@@ -63,15 +62,15 @@ get_colours_ASITIS <- function(n = NA, dark = F){
 #' @export
 get_colors_ASITIS_marketing <- function(){
   colors_ASITIS_marketing <- list(
-    "hlavní modrá" <- "#00344b",
-    "hlavní oranžová" <- "#d46515",
-    "Doplňkové barvy produktových značek" <- c("#5ec3ad", "#19abde", "#ec548d"),
-    "Doplňkové barvy zeleno-modré" <- c("#5ec3ad", "#4bc3c8", "#35c3e1", "#19abde", "#186ba1", "#142458"),
-    "Doplňkové barvy růžovo-modré" <- c("#e97369", "#ec548d", "#cb539f", "#9657a3", "#754b9e", "#2e2468"),
-    "Doplňkové barvy žluto-červené" <- c("#eabe3b", "#ee9a3a", "#ef7e32", "#de542d", "#bf2324", "#801517"),
-    "Barvy webových stránek oranžové" <- c("#fbf0e8", "#edc09f", "#e6a677", "#db7f3d", "#d46515", "#94470f", "#813e0d"),
-    "Barvy webových stránek modré" <- c("#e6ebed", "#96acb5", "#6b8997", "#2b576a", "#00344b", "#002435", "#00202e"),
-    "Barvy webových stránek neutrální" <- c("#eeeeef", "#bbbbbc", "#9e9fa0", "#747577", "#58595b", "#3e3e40", "#363638")
+    "hlavní modrá" = "#00344b",
+    "hlavní oranžová" = "#d46515",
+    "Doplňkové barvy produktových značek" = c("#5ec3ad", "#19abde", "#ec548d"),
+    "Doplňkové barvy zeleno-modré" = c("#5ec3ad", "#4bc3c8", "#35c3e1", "#19abde", "#186ba1", "#142458"),
+    "Doplňkové barvy růžovo-modré" = c("#e97369", "#ec548d", "#cb539f", "#9657a3", "#754b9e", "#2e2468"),
+    "Doplňkové barvy žluto-červené" = c("#eabe3b", "#ee9a3a", "#ef7e32", "#de542d", "#bf2324", "#801517"),
+    "Barvy webových stránek oranžové" = c("#fbf0e8", "#edc09f", "#e6a677", "#db7f3d", "#d46515", "#94470f", "#813e0d"),
+    "Barvy webových stránek modré" = c("#e6ebed", "#96acb5", "#6b8997", "#2b576a", "#00344b", "#002435", "#00202e"),
+    "Barvy webových stránek neutrální" = c("#eeeeef", "#bbbbbc", "#9e9fa0", "#747577", "#58595b", "#3e3e40", "#363638")
   )
   return(colors_ASITIS_marketing)
 }
